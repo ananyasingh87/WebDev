c2932dea299fbf983ac849c590d13873d963c9c6
ananyasingh87 <53980756+ananyasingh87@users.noreply.github.com>
2020-12-03 16:49:37 +0530

    version-1

---
+++ portfolio-1/mywebsite/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from . import models
+
+# Register your models here.
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description')
+    list_filter = ('title', )
+    ordering = ('title', )
+    search_field = ('title')
+admin.site.register(models.Project, ProjectAdmin)
